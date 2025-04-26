8e3f52678eb94b943cf78f28be739077b068552e
ro care <sachhidanand.3dlogic@gmail.com>
2025-04-26 23:00:46 +0530

    added

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ function home() {
             {/*=================hero section================================== */}
             <div className="hero-section flex items-center  justify-around">
                 <div className="leftHeroPortion w-7/12 py-2.5 flex items-center justify-end">
-                    <img src="assets/images/BannerImg2.jpg" alt="" />
+                    <img src="/assets/images/BannerImg2.jpg" alt="" />
                 </div>
                 <div className="rightHeroDetails w-5/12 flex flex-col items-start justify-between  pl-2 pr-2">
                     <div>
@@ -49,7 +49,7 @@ function home() {
             </div> */}
 
             <div className=' serviceBannerSection'>
-                <img src="assets/images/home-appliaces.webp" alt='Service Banner' className='serviceBanner'  />
+                <img src="/assets/images/home-appliaces.webp" alt='Service Banner' className='serviceBanner'  />
             </div>
 
             <ServiceSection />
@@ -72,7 +72,7 @@ function home() {
 
                 <div className='flex justify-center items-start'>
                     <h3 className='text-center chooseStar  '>Why Choose Us</h3>
-                    <span><img src="assets/images/star.png" alt="" className='w-10' /></span>
+                    <span><img src="/assets/images/star.webp" alt="" className='w-10' /></span>
                 </div>
 
                 <div className="chooseSplits">
