9f13be582172f7e80dafd2cdda8fadc57c58feaf
ro care <sachhidanand.3dlogic@gmail.com>
2025-04-26 23:59:50 +0530

    heheh

--- src/app/components/servicesSection/servicesSection.jsx
+++ src/app/components/servicesSection/servicesSection.jsx
@@ -9,16 +9,16 @@ function ServiceSection() {
     const [showAllServices, setShowAllServices] = useState(false);
 
     const Services = [
-        { id: 1, name: 'Ro Service', image: "assets/images/serviceBrands/RoInstallation.png", info: 'Complete maintenance to keep your purifier running smoothly' },
-        { id: 9, name: 'Washing Machine', image: "assets/images/servicesImages/washing machine.png", info: '' },
-        { id: 6, name: 'Refrigerator', image: "assets/images/servicesImages/refrigerator.png", info: 'Keep your food fresh and beverages cool with our energy-efficient refrigerators, designed with advanced cooling technology and spacious interiors.' },
-        { id: 7, name: 'Air Conditioners', image: "assets/images/servicesImages/ac.png", info: 'Stay cool during the hottest days with our powerful and silent air conditioners. Fast cooling, energy-saving, and built for long-lasting comfort.' },
-        { id: 8, name: 'Geyser', image: "assets/images/serviceBrands/geyser icon 70x70.png", info: 'Enjoy instant hot water with our high-performance geysers. Designed for safety, durability, and efficient heating to keep your winters warm and cozy.' },
-        { id: 10, name: 'Microwave', image: "assets/images/servicesImages/microWave.png", info: '' },
-        { id: 11, name: 'Led', image: "assets/images/servicesImages/led.png", info: '' },
-        { id: 12, name: 'kitchen Chimney', image: "assets/images/servicesImages/kitchen chimney.png", info: '' },
-        { id: 13, name: 'Air Purifier', image: "assets/images/servicesImages/air cooler.png", info: '' },
-        { id: 14, name: 'Vaccum Cleaner', image: "assets/images/servicesImages/vacuum cleaner.png", info: '' }
+        { id: 1, name: 'Ro Service', image: "/assets/images/serviceBrands/RoInstallation.png", info: 'Complete maintenance to keep your purifier running smoothly' },
+        { id: 9, name: 'Washing Machine', image: "/assets/images/servicesImages/washing machine.png", info: '' },
+        { id: 6, name: 'Refrigerator', image: "/assets/images/servicesImages/refrigerator.png", info: 'Keep your food fresh and beverages cool with our energy-efficient refrigerators, designed with advanced cooling technology and spacious interiors.' },
+        { id: 7, name: 'Air Conditioners', image: "/assets/images/servicesImages/ac.png", info: 'Stay cool during the hottest days with our powerful and silent air conditioners. Fast cooling, energy-saving, and built for long-lasting comfort.' },
+        { id: 8, name: 'Geyser', image: "/assets/images/serviceBrands/geyser icon 70x70.png", info: 'Enjoy instant hot water with our high-performance geysers. Designed for safety, durability, and efficient heating to keep your winters warm and cozy.' },
+        { id: 10, name: 'Microwave', image: "/assets/images/servicesImages/microWave.png", info: '' },
+        { id: 11, name: 'Led', image: "/assets/images/servicesImages/led.png", info: '' },
+        { id: 12, name: 'kitchen Chimney', image: "/assets/images/servicesImages/kitchen chimney.png", info: '' },
+        { id: 13, name: 'Air Purifier', image: "/assets/images/servicesImages/air cooler.png", info: '' },
+        { id: 14, name: 'Vaccum Cleaner', image: "/assets/images/servicesImages/vacuum cleaner.png", info: '' }
     ];
 
     const handleServiceClick = (serviceName) => {
